05e1abf45190fb3116f429224cf076d5f569c9b8
root <root@localhost.localdomain>
2019-03-16 22:36:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# AUTO-RECON
+## Easy to use script to quickly scan a Target in Kali Linux
+
+![alt text](https://github.com/Knowledge-Wisdom-Understanding/Auto-Recon/blob/master/auto-recon.PNG)
+
+
+### INSTALLATION
+```
+cd /opt
+git clone https://github.com/Knowledge-Wisdom-Understanding/Auto-Recon.git
+cd Auto-Recon
+chmod +x setup.sh
+./setup.sh
+chmod +x recon.sh
+```
+
+### Usage:
+```
+./recon.sh
+```
+
+
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo '[+] Running: apt-get update -y'
+apt-get update -y
+
+echo '[+] Downloading dirsearch repository in /opt folder'
+cd /opt
+git clone https://github.com/maurosoria/dirsearch.git
+
+echo '[+] Downloading & Installing penetest-machine repository in /opt folder'
+cd /opt
+git clone https://github.com/DanMcInerney/pentest-machine.git
+cd pentest-machine
+./setup.sh
+
+echo '[+] Congratulations, All tools installed successfully!'
+echo '[+] Done. When you are done running this tool, cd into pentest-machine and deactivate the virtualenv by running the command: source /opt/pentest-machine/pm/deactivate'
